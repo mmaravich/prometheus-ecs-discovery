8d93a746658b93037a6f3bf56d3b07e70078e36b
Mladen Maravic <mladen.maravic@curl.services>
2018-07-26 11:24:18 +0200

    Removing labels we don't need

--- main.go
+++ main.go
@@ -207,14 +207,8 @@ func (t *AugmentedTask) ExporterInformation() []*PrometheusTaskInfo {
 
 		labels := yaml.MapSlice{}
 		labels = append(labels,
-			yaml.MapItem{"task_arn", *t.TaskArn},
-			yaml.MapItem{"task_name", *t.TaskDefinition.Family},
-			yaml.MapItem{"task_revision", fmt.Sprintf("%d", *t.TaskDefinition.Revision)},
-			yaml.MapItem{"task_group", *t.Group},
-			yaml.MapItem{"cluster_arn", *t.ClusterArn},
 			yaml.MapItem{"container_name", *i.Name},
-			yaml.MapItem{"container_arn", *i.ContainerArn},
-			yaml.MapItem{"docker_image", *d.Image},
+			yaml.MapItem{"task_name", *t.TaskDefinition.Family},
 			yaml.MapItem{"__metrics_path__", *metricPath},
 		)
 		ret = append(ret, &PrometheusTaskInfo{
